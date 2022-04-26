a964c7fd0bf05c28f2763036122cd60445201f43
AndresChouela <101569013+AndresChouela@users.noreply.github.com>
2022-04-26 09:06:18 -0300

    Initial commit

---
+++ Assets/ejercicio0.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ejercicio0 : MonoBehaviour {
+    public int num1;
+    public int num2;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public void validarcontrasenia()
+    {
+        int contador = 1;
+        //for(int i = 0, i > 3, i ++){
+
+        if (num2 < num1)
+        {
+            while (num1 >= num2)
+            {
+                Debug.Log(num1);
+                num1--;
+            }
+        }
+        else if (num1 == num2)
+        {
+            Debug.Log("los numeros son iguales");
+        }
+        else if (num1 < num2)
+        {
+            Debug.Log("el segundo numero es mayor que el primero");
+        }
+    }
+}
